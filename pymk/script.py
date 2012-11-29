70c78ab481ca6d4233ae85747e5230d3f9c67b00
Dominik "Socek" Długajczyk <msocek@gmail.com>
2012-11-29 21:54:49 +0100

    Improvment of cmd parsing.

--- pymk/script.py
+++ pymk/script.py
@@ -5,9 +5,22 @@ from pymk.task import TASKS
 from pymk.error import NoMkfileFound, CommandError
 
 def run():
+    cmd = {
+        'tasks' : [],
+        'options' : [],
+    }
+    def parse_command():
+        for element in sys.argv[1:]:
+            if element.startswith('-'):
+                cmd['options'].append(element[1:])
+            else:
+                cmd['tasks'].append(element)
     def start_loggin():
         FORMAT = '%(message)s'
-        logging.basicConfig(level=logging.INFO, format=FORMAT)
+        if 'd' in cmd['options']:
+            logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+        else:
+            logging.basicConfig(level=logging.INFO, format=FORMAT)
         return logging.getLogger('pymk')
     def append_python_path():
         sys.path.append(os.getcwd())
@@ -16,20 +29,21 @@ def run():
             logger.info("No mkfile.py file found!")
             raise NoMkfileFound()
         import mkfile
-    def parse_command():
+    def run_tasks():
         if len(sys.argv) == 1:
             text = 'Avalible tasks:\n\t'
             text += '\n\t'.join(TASKS.keys())
             logger.info(text)
         else:
-            for task in sys.argv[1:]:
+            for task in cmd['tasks']:
                 TASKS[task]().run()
     #---------------------------------------------------------------------------
     try:
+        parse_command()
         logger = start_loggin()
         append_python_path()
         import_mkfile()
-        parse_command()
+        run_tasks()
     except NoMkfileFound:
         return 1
     except CommandError:
