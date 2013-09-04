fdb05d762bf81a5719625f6e88017906c3aec7f6
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2013-09-04 20:45:40 +0200

    Refactored compere_version.

--- pymk/__init__.py
+++ pymk/__init__.py
@@ -10,7 +10,4 @@ def compare_version(a, b, separator='.', ignorecase=True):
         return [int(x) if x.isdigit() else [int(y) if y.isdigit() else y for y in re.findall("\d+|[a-zA-Z]+", x)] for x in v.split(separator)]
     a = _preprocess(a, separator, ignorecase)
     b = _preprocess(b, separator, ignorecase)
-    try:
-        return (a > b) - (a < b)
-    except:
-        return False
+    return (a > b) - (a < b)
